7a50bef1f3fd20b8ec2af52877a8000151838e39
YoungJun Jo <dudwns0925@naver.com>
2020-07-11 12:15:47 +0900

    nginx install

---
+++ nginx_install.sh
@@ -0,0 +1,12 @@
+yum groupinstall "Development Tools"
+yum install pcre pcre-devel
+yum install zlib zlib-devel
+yum install openssl openssl-devel
+mkdir /home/src
+cd /home/src/
+wget http://nginx.org/download/nginx-1.18.0.tar.gz
+tar xvfz nginx-1.18.0.tar.gz
+cd nginx-1.18.0
+./configure --prefix=/etc/nginx
+make
+make install
